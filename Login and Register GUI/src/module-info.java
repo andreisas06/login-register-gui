7760a3e548d0aeb5df74a72edfaa4266eaf1f521
Andrei Sas <andreisas007@gmail.com>
2020-11-08 19:28:29 +0100

    fixed mysql connection

--- Login and Register GUI/src/module-info.java
+++ Login and Register GUI/src/module-info.java
@@ -4,7 +4,7 @@ module Login.and.Register.GUI {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.sql;
-    //requires mysql.connector.java;
+    requires mysql.connector.java;
     //JavaFX Tutorial | Login and Register Screen GUI In IntelliJ & Scene Builder With MySQL - Part 1|2 (min 17)
     opens sample;
 
