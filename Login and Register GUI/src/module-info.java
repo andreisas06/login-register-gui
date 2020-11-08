724d9c4bdad6a601fa76f18105c8a197cc081432
Andrei Sas <andreisas007@gmail.com>
2020-11-08 19:17:33 +0100

    minor improvements

---
+++ Login and Register GUI/src/module-info.java
@@ -0,0 +1,11 @@
+module Login.and.Register.GUI {
+
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    //requires mysql.connector.java;
+    //JavaFX Tutorial | Login and Register Screen GUI In IntelliJ & Scene Builder With MySQL - Part 1|2 (min 17)
+    opens sample;
+
+}
